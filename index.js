ab321ada22a6ab73646ceaa0b9ec602ab9e851f8
Nobert-Muma <mumanobert17@gmail.com>
2024-07-22 16:39:49 +0300

    added functionality for checking out if a word is a palindrome

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let stringToLowercase=word.toLowerCase();
+  let reversedString=stringToLowercase.split('').reverse().join('');
+  if(stringToLowercase===reversedString){
+    return true;
+  } else {
+    return false;
+  }
 }
-
+console.log(isPalindrome('racecar'));
+console.log(isPalindrome('robot'));
 /* 
   Add your pseudocode here
+  1. create a variable to hold the lowercased string
+  2. convert the string to lowercase
+  3. create a variable to hold the reversed version of the string
+  4. use if else statement to complare the string and its reversed version to check if they are palindrome
 */
 
 /*
